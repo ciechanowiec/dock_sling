4f8fc8c5c2e7db01412593ad40d71c9a79f46ee5
Herman Ciechanowiec <herman@ciechanowiec.eu>
2024-06-19 23:06:31 +0200

    Add run modes support

--- src/2_sling/sling-starter.sh
+++ src/2_sling/sling-starter.sh
@@ -3,5 +3,6 @@
 # exec is required in order to set the Java process as PID 1 inside the container, since Docker sends
 # termination signals only to PID 1, and we need those signals to be handled by the java process:
 SLING_DIR=${SLING_DIR:-/opt/sling}
+RUN_MODES=${RUN_MODES:-basic,sling}
 cd "$SLING_DIR" || exec 1
-exec "$SLING_DIR/bin/launcher" -f "$SLING_DIR/sling-12.far"
+exec "$SLING_DIR/bin/launcher" -D sling.run.modes="$RUN_MODES" -f "$SLING_DIR/sling-12.far"
